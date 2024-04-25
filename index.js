257a4718dd13588e4f158196c16906d9f03faaaa
Adrian Martin <martinolivaadrian@gmail.com>
2024-04-25 20:32:51 +0200

    first two iterations done/going to check React intro

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let i = 0;
+    while (i < this.length && this.items[i] < item) {
+      i++;
+    }
+    this.items.splice(i, 0, item);
+    this.length++;
+  }
 
   get(pos) {}
 
